1c0c302d4451d2c2889601f2f36dea731d9736ee
Irakli <irakli.gvelesiani.1@btu.edu.ge>
2021-02-06 03:56:24 +0400

    foody

---
+++ README.md.txt
@@ -0,0 +1,7 @@
+The Foody delivery app is a mobile app that allows customers to place a food order from any of the restaurants in an area using their smartphone or tablet device.With a simple payment system and interface.
+
+
+
+
+
+©Irakli Gvelesiani
--- app/src/main/java/com/example/authentication/DashboardActivity.kt
+++ app/src/main/java/com/example/authentication/DashboardActivity.kt
@@ -1,11 +1,4 @@
 package com.example.authentication
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class DashboardActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-    }
+class DashboardActivity {
 }
